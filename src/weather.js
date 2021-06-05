72ccf57ec2aa29e6db1fda50e95df2cb4d076889
aliciaECodes <aebranscomb@gmail.com>
2021-06-04 22:39:47 -0500

    Updated styling in CSS (fonts, colors)

--- src/weather.js
+++ src/weather.js
@@ -105,7 +105,7 @@ function displayWeather(response) {
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity + "%";
-  windElement.innerHTML = Math.round(response.data.wind.speed) + " km/h";
+  windElement.innerHTML = Math.round(response.data.wind.speed) + " mph";
   iconElement.setAttribute(
     "src",
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
